ea6181378e433d0c53ff40fd3c02f63f36f23e82
enury <elisa.nury@gmail.com>
2024-05-28 11:26:22 +0200

    data corrected/improved, HoverBubble modification

--- src/map/HoverBubble.jsx
+++ src/map/HoverBubble.jsx
@@ -20,8 +20,11 @@ const Hover = props => {
     <div
       className="p6o-map-hover"
       style={style}>
-      {colocated > 0 ?
-        <>{node.geometry.label ? node.geometry.label : node.properties.title} <span className="p6o-map-hover-colocated">{colocated+1} items</span></> : <>{node.geometry.label ? node.geometry.label : node.properties.title}</> 
+      {colocated > 0 && node.geometry.label ?
+        <>{node.geometry.label} <span className="p6o-map-hover-colocated">{colocated+1} items</span></> 
+        : node.geometry.label ? <>{node.geometry.label}</> 
+        : colocated > 0 ? <>{node.properties.title} <span className="p6o-map-hover-colocated">+ {colocated} more</span></> 
+        : <>{node.properties.title}</> 
       }
       {node.geometry?.granularity &&
         <div className="p6o-map-hover-granularity">
